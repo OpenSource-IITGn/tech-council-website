18de0c0ce5bcd2c961edd9f56954d05cf4f774a1
Shivam Chaudhary <shivamc021999@gmail.com>
2022-08-08 20:20:09 +0530

    Contact page corrections

--- src/Constants/routes.js
+++ src/Constants/routes.js
@@ -1,10 +1,10 @@
 const ROUTES = [
-  { name: "Home", path: "/" },
-  { name: "About", path: "/About" },
-  { name: "Clubs / Hobby groups", path: "/Clubs" },
-  { name: "Contact", path: "/Contact" },
-  { name: "Events", path: "/Events" },
-  { name: "Intern Portal", path: "/Internships" },
+	{ name: "Home", path: "/" },
+	{ name: "About", path: "/About" },
+	{ name: "Clubs / Hobby groups", path: "/Clubs" },
+	{ name: "Events", path: "/Events" },
+	{ name: "Intern Portal", path: "/Internships" },
+	{ name: "Contact", path: "/Contact" },
 ];
 
 export default ROUTES;
