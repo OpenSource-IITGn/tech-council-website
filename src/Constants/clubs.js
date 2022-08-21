f1d6c113bf848653de8ec5a1101fd185c1e23531
Shivam Chaudhary <shivamc021999@gmail.com>
2022-08-21 20:52:38 +0530

    clubs page desktop version done

---
+++ src/Constants/clubs.js
@@ -0,0 +1,235 @@
+const CLUBS = [
+	{
+		id: 0,
+		name: "METIS",
+		desc: "Coding Club, at IITGN aims at spreading knowledge related to computer programming to the students and also develop websites and applications that cater to the needs of the institute. The club arranges weekly workshops focusing on basic programming and designing languages like Java and HTML. ",
+		banner: "/assets/images/club_logo/Metis_Logo.jpeg",
+		backgroundColor: "",
+		social: [
+			{
+				id: 0,
+				name: "Email",
+				src: "",
+				icon: "/assets/icons/google-48.png",
+			},
+			{
+				id: 1,
+				name: "Youtube",
+				src: "",
+				icon: "/assets/icons/youtube-48.png",
+			},
+		],
+	},
+	{
+		id: 1,
+		name: "MEAN MECHANICS",
+		desc: "Believing in the philosophy of 'Learning through Application', the mean Mechanics club plays a crucial role in inspiring and creating curiosity towards robotics and mechanics. It organizes workshops and competitions throughout the year focusing on different aspects of engineering and giving hands on experience to participants.",
+		banner: "/assets/images/club_logo/MeanMech Logo.png",
+		backgroundColor: "",
+		social: [
+			{
+				id: 0,
+				name: "Email",
+				src: "",
+				icon: "/assets/icons/google-48.png",
+			},
+			{
+				id: 1,
+				name: "Youtube",
+				src: "",
+				icon: "/assets/icons/youtube-48.png",
+			},
+		],
+	},
+	{
+		id: 2,
+		name: "DIGIS",
+		desc: "DigiS is the digital sports development club of IIT Gandhinagar. It acts as a platform for creative thinkers to turn their imaginations into reality. The club intends to take human-technology interaction to a whole new level. The club develops simulations, applications, augmented reality platforms for android and OS systems.",
+		banner: "/assets/images/club_logo/DiGiS_logo.jpg",
+		backgroundColor: "",
+		social: [
+			{
+				id: 0,
+				name: "Email",
+				src: "",
+				icon: "/assets/icons/google-48.png",
+			},
+			{
+				id: 1,
+				name: "Youtube",
+				src: "",
+				icon: "/assets/icons/youtube-48.png",
+			},
+		],
+	},
+	{
+		id: 3,
+		name: "ODYSSEY",
+		desc: "The universe is as vast as our imagination. The Odyssey club of IITGN attempts to foster the knowledge and participation of students of the institute in the field of astronomy. It encourages students across different disciplines and programmes to relive their passion to learn about the space and universe as a whole.",
+		banner: "/assets/images/club_logo/DiGiS_logo.jpg",
+		backgroundColor: "",
+		social: [
+			{
+				id: 0,
+				name: "Email",
+				src: "",
+				icon: "/assets/icons/google-48.png",
+			},
+			{
+				id: 1,
+				name: "Youtube",
+				src: "",
+				icon: "/assets/icons/youtube-48.png",
+			},
+		],
+	},
+	{
+		id: 4,
+		name: "ML HOBBY GROUP",
+		desc: "Exposing the student community to the growing field of Machine learning and Deep learning, this group conducts peer led learning sessions, organizing and being part of ML hackathons. It also provides opportunities in the Machine Learning and Deep Learning fields.",
+		banner: "/assets/images/club_logo/DiGiS_logo.jpg",
+		backgroundColor: "",
+		social: [
+			{
+				id: 0,
+				name: "Email",
+				src: "",
+				icon: "/assets/icons/google-48.png",
+			},
+			{
+				id: 1,
+				name: "Youtube",
+				src: "",
+				icon: "/assets/icons/youtube-48.png",
+			},
+		],
+	},
+	{
+		id: 5,
+		name: "WEB3/BLOCKCHAIN HOBBY GROUP",
+		desc: "Exposes members to this growing technology by organizing training activities, workshops etc. in this field.Conducting workshops where they would be providing in-depth knowledge on blockchains, their development in languages like Solidity, Rust etc., and teaching smart contracts.",
+		banner: "/assets/images/club_logo/DiGiS_logo.jpg",
+		backgroundColor: "",
+		social: [
+			{
+				id: 0,
+				name: "Email",
+				src: "",
+				icon: "/assets/icons/google-48.png",
+			},
+			{
+				id: 1,
+				name: "Youtube",
+				src: "",
+				icon: "/assets/icons/youtube-48.png",
+			},
+		],
+	},
+	{
+		id: 6,
+		name: "ESPORTS WING",
+		desc: "Organizes Gaming Nights, conducts Gaming Tournaments. Additionally, it also collaborates with other e-sports brands.",
+		banner: "/assets/images/club_logo/IITGN esports.jpg",
+		backgroundColor: "#B9783C",
+		social: [
+			{
+				id: 0,
+				name: "Email",
+				src: "",
+				icon: "/assets/icons/google-48.png",
+			},
+			{
+				id: 1,
+				name: "Youtube",
+				src: "",
+				icon: "/assets/icons/youtube-48.png",
+			},
+		],
+	},
+	{
+		id: 7,
+		name: "HARDWARE, IoT, VLSI GROUP",
+		desc: "To create a platform to apply the knowledge gained in theory classes to solving real-world problems. Planning and overseeing training activities and hands-on sessions to gain expertise in industry-relevant concepts and skills like IoT and Embedded Systems.",
+		banner: "/assets/images/club_logo/IITGN esports.jpg",
+		backgroundColor: "#B9783C",
+		social: [
+			{
+				id: 0,
+				name: "Email",
+				src: "",
+				icon: "/assets/icons/google-48.png",
+			},
+			{
+				id: 1,
+				name: "Youtube",
+				src: "",
+				icon: "/assets/icons/youtube-48.png",
+			},
+		],
+	},
+	{
+		id: 8,
+		name: "ANVESHANAM",
+		desc: "Promoting general awareness about research - Promoting software learning and conducting guest Interaction sessions. Providing career guidance, increasing awareness about scholarships and research events, interactions and collaborations abroad.",
+		banner: "/assets/images/club_logo/AnveshanamLogoPNG.png",
+		backgroundColor: "#FFF",
+		social: [
+			{
+				id: 0,
+				name: "Email",
+				src: "",
+				icon: "/assets/icons/google-48.png",
+			},
+			{
+				id: 1,
+				name: "Youtube",
+				src: "",
+				icon: "/assets/icons/youtube-48.png",
+			},
+		],
+	},
+	{
+		id: 9,
+		name: "GRASP",
+		desc: "Organize events, workshops, and activities as part of the council. Hosting Inter-college and Intra-college contests. Interview Preparation.",
+		banner: "/assets/images/club_logo/GRASP_logo.png",
+		backgroundColor: "#0F054C",
+		social: [
+			{
+				id: 0,
+				name: "Email",
+				src: "",
+				icon: "/assets/icons/google-48.png",
+			},
+			{
+				id: 1,
+				name: "Youtube",
+				src: "",
+				icon: "/assets/icons/youtube-48.png",
+			},
+		],
+	},
+	{
+		id: 10,
+		name: "TIKERER'S LAB",
+		desc: "Organize events, workshops, and activities as part of the council. Hosting Inter-college and Intra-college contests. Interview Preparation.",
+		banner: "/assets/images/club_logo/TL_Logo.png",
+		backgroundColor: "",
+		social: [
+			{
+				id: 0,
+				name: "Email",
+				src: "",
+				icon: "/assets/icons/google-48.png",
+			},
+			{
+				id: 1,
+				name: "Youtube",
+				src: "",
+				icon: "/assets/icons/youtube-48.png",
+			},
+		],
+	},
+];
+
+export default CLUBS;
